dc92fc1e2d598939c38e151345ea093cc7838147
MikhailTapio <mikhailtapio13468@163.com>
2022-08-16 15:37:44 +0800

    Initialization

---
+++ src/main/scala/committee/nova/scift/ScalaAdapter.scala
@@ -0,0 +1,21 @@
+package committee.nova.scift
+
+import org.dimdev.riftloader.listener.Instantiator
+
+class ScalaAdapter extends Instantiator {
+  override def newInstance[T](clazz: Class[T]): T = {
+    val name = clazz.getName
+    try {
+      val objectClass = Class.forName(name + "$")
+      val moduleField = objectClass.getField("MODULE$")
+      val instance = moduleField.get(null).asInstanceOf[T]
+      if (instance == null) throw new NullPointerException
+      instance
+    } catch {
+      case _: Exception => {
+        println(s"Unable to find ${name}$$MODULE$$")
+        clazz.newInstance()
+      }
+    }
+  }
+}
---
+++ src/main/scala/committee/nova/scift/Scift.scala
@@ -0,0 +1,9 @@
+package committee.nova.scift
+
+import org.dimdev.riftloader.listener.InitializationListener
+
+object Scift extends InitializationListener {
+  override def onInitialization(): Unit = {
+
+  }
+}
